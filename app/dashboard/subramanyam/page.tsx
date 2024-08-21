1636f0b4f00fc6fa344d3a96728cfccf2fbd8111
Arunteja2345 <aruntejapulaparthi9@gmail.com>
2024-08-21 20:09:22 +0530

    removed

--- app/dashboard/subramanyam/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Page(){
-    return <p>
-        <img src="https://scontent.cdninstagram.com/v/t51.2885-19/347248768_780246363794280_404576429898044455_n.jpg?stp=dst-jpg_s100x100&_nc_cat=111&ccb=1-7&_nc_sid=fcb8ef&_nc_ohc=w1zpbpNCia0Q7kNvgFFzcN1&_nc_ad=z-m&_nc_cid=1174&_nc_ht=scontent.cdninstagram.com&oh=00_AYBNMhfLyZcZQhivmgfy5ajYZaMuq3IDi2F1nuSYIfYCYQ&oe=66CB5369" alt="subrahmany image" />
-    </p>;
-}
